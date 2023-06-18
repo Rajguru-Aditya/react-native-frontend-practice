0a28d9c2bd101ada9d3f987336e039f12eff3b3a
Aditya Rajguru <77228474+Rajguru-Aditya@users.noreply.github.com>
2023-06-19 00:35:46 +0530

    Add recommendation products

--- Components/Recommendations/Recommendations.tsx
+++ Components/Recommendations/Recommendations.tsx
@@ -1,13 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import styles from './styles'
+import AntIcons from 'react-native-vector-icons/AntDesign'
 
 const Recommendations = () => {
+
+  const products = [
+    {
+      name: 'Comfy Chair',
+      seller: 'Walter Knoll',
+      cost: 200,
+      img: "https://images.unsplash.com/photo-1580480055273-228ff5388ef8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      name: 'Night Lamp',
+      seller: 'Walter Knoll',
+      cost: 150,
+      img: "https://images.unsplash.com/photo-1588732282235-d4a99e41d0d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
+    },
+  ]
+
+  const RecommendedProduct = ({name, seller, cost, img}) => (
+    <View style={styles.recommendedProductContainer}>
+      <AntIcons name="heart" size={20} color="#C74644" style={styles.recommendationHeart} />
+      <View style={styles.recommendationImgContainer}>
+        <Image source={{uri: img}} style={styles.recommendationImage} borderRadius={20}/>
+      </View>
+      <View style={styles.recommendationTextContainer}>
+        <Text style={styles.recommendationName}>{name}</Text>
+        <Text style={styles.recommendationSeller}>{seller}</Text>
+        <Text style={styles.recommendationPrice}>${cost}</Text>
+      </View>
+    </View>
+  )
+
   return (
     <View style={styles.container}>
       <View style={styles.recommendationContainerHeader}>
         <Text style={styles.headerText}>Recommendations</Text>
         <Text style={styles.seeAllText}>See All</Text>
+      </View>
+      <View style={styles.recommendationContainerBody}>
+        {
+          products.map((product, index) => (
+            <RecommendedProduct key={index} name={product.name} seller={product.seller} cost={product.cost} img={product.img} />
+            )
+          )
+        }
       </View>
     </View>
   )
--- Components/Recommendations/styles.tsx
+++ Components/Recommendations/styles.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -20,6 +20,68 @@ const styles = StyleSheet.create({
   recommendationContainerHeader: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginBottom: 15,
+  },
+
+  recommendationContainerBody: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  recommendedProductContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: 10,
+    position: 'relative',
+    width: Dimensions.get('window').width / 2 - 20,
+    backgroundColor: '#fff',
+  },
+
+  recommendationImgContainer: {
+    width: "100%",
+    height: 200,
+    borderRadius: 15,
+    overflow: 'hidden',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+
+  recommendationImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+
+  recommendationTextContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+
+  recommendationHeart: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    zIndex: 1,
+    elevation: 10,
+  },
+
+  recommendationName: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#222',
+  },
+
+  recommendationSeller: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#888',
+  },
+
+  recommendationPrice: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#C87F45',
   },
 })
 
